36796e881cc77f7935e5be8617c69c6e77de5196
Albert <warhammer431@mail.ru>
2022-08-03 08:36:15 +0300

    fix userPerformance

--- src/components/UserPerformance/ui.tsx
+++ src/components/UserPerformance/ui.tsx
@@ -7,15 +7,34 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export const UserPerformance = () => {
     const [active, setActive] = React.useState(false)
-
+    let menuEnterTimer, menuLeaveTimer;
     const props = useSpring({
-        display: active ? 'block' : 'none'
-      });
+        display: active ? 'block' : 'none',
+        top: active ? '55px' : '-10px',
+        color: active ? '#F1C40F' : 'inherit'
+    });
 
+    const leaveMouse = () => {
+        clearTimeout(menuEnterTimer);
+        menuLeaveTimer = window.setTimeout(() => {
+            setActive(false)
+        }, 400);
+    }
+
+    const enterMouse = () => {
+        clearTimeout(menuLeaveTimer);
+        menuEnterTimer = window.setTimeout(() => {
+            setActive(true)
+        }, 0);
+    }
+
+    React.useEffect(()=>{
+
+    }, [active])
 
     return (    
-        <div className='userPerformance' onMouseEnter={() => setActive(true)}
-             onMouseLeave={() => setActive(false)}>
+        <div className='userPerformance' onMouseEnter={enterMouse}
+             onMouseLeave={leaveMouse}>
             <div className='userPerformance__icon'>
                 <img className='userPerformance__icon-picture' src={user} alt="" />
                 <div className='userPerformance__icon-notification'>1</div>
@@ -23,7 +42,7 @@ export const UserPerformance = () => {
             <animated.div
                 style={{
                     display: props.display,
-                    transition: '1s'
+                    top: props.top
                 }}
                 className="userPerformance__menu"
             >
@@ -32,7 +51,7 @@ export const UserPerformance = () => {
             </animated.div>
             <div className='userPerformance__info'>
                 <div className='userPerformance__info-name'>
-                    <Typography variant='h4'>Albert</Typography>
+                    <Typography variant='h4' style={{color: active ? '#F1C40F' : 'inherit'}}>Albert</Typography>
                     <ArrowDropDownIcon />
                 </div>
                 <Typography variant='h5'>warhammer431@mail.ru</Typography>
