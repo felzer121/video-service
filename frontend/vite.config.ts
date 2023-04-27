e469e7b0054ac2b4015cdbcefe116f5a45f7b305
Albert <a.teregulov@bet2u.com>
2023-04-27 10:48:36 +0300

    fix problem tsx with import as component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import svgr from '@honkhonk/vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [svgr(), react(), tsconfigPaths()],
-  resolve: {
-    
-  },
-})
+  resolve: {},
+});
